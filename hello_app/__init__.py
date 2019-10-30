061666f3c8ce4e7ab5b4fbfe9a125bafc1d95903
Vivek Dwivedi <thevivekdwivedi@gmail.com>
2019-10-30 16:50:27 -0400

    Adding keyvault config
    Signed-off-by: Vivek Dwivedi <thevivekdwivedi@gmail.com>

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,2 +1,8 @@
 from flask import Flask  # Import the Flask class
 app = Flask(__name__)    # Create an instance of the class for our use
+
+app.config.update(
+    AZURE_CLIENT_ID='92ea35ba-dae9-4731-941c-51bbd2f7dac6',
+    AZURE_TENANT='ea7bec13-76eb-4f00-ad1a-4da88023607f',
+    AZURE_SECRET='qw?ppzdARMd5t6qfly[MQNs?8oKMyi4='
+)
